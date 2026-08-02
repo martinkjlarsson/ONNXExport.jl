ff254b94f873e9c8179b0944b6b36d38cd54cef2
Martin Larsson <martin.larsson@math.lth.se>
2026-08-02 09:51:44 +0200

    Add temporary fix for symbolic dims in pooling

--- ext/NNlibExt/pooling.jl
+++ ext/NNlibExt/pooling.jl
@@ -1,3 +1,17 @@
+# Since PoolDims does not use the last dimension of x, we can hack a fix to allow symbolic
+# batch dimensions. To fully support symbolic dimensions, the functions below need other
+# methods not relying on PoolDims.
+const Last{S,T} = Union{
+    Tuple{S},Tuple{T,S},Tuple{T,T,S},Tuple{T,T,T,S},Tuple{T,T,T,T,S},Tuple{T,T,T,T,T,S}
+}
+function NNlib.PoolDims(
+    x_size::Last{ProbeNumber,Int}, k::Union{NTuple{L,Int},Int}; kwargs...
+) where {L}
+    M = length(x_size)
+    x_size = ntuple(i -> i == M ? -1 : Int(x_size[i]), M)
+    return PoolDims(x_size, k; kwargs...)
+end
+
 function NNlib.maxpool(x::ProbeArray{T,N}, pdims::PoolDims) where {N,T}
     new_dims = pool_output_size(x, pdims)
     if pool_is_global(pdims)
@@ -31,7 +45,7 @@ function NNlib.lpnormpool(x::ProbeArray{T,N}, pdims::PoolDims; p::Real) where {N
 end
 
 function pool_output_size(x, pdims)
-    return NNlib.output_size(pdims)..., NNlib.channels_out(pdims), NNlib.size(x, ndims(x))
+    return NNlib.output_size(pdims)..., NNlib.channels_out(pdims), raw_size(x, ndims(x))
 end
 
 pool_is_global(pdims) = NNlib.input_size(pdims) == NNlib.kernel_size(pdims)
